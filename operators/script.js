d1c14d7454fb5f9d85920cca7a2b9cbefadc317f
hritikd3 <hritikdangi3@gmail.com>
2022-07-21 15:52:59 +0530

    Js operators

---
+++ operators/script.js
@@ -0,0 +1,29 @@
+// Math operators
+
+const ageHritik = 2020 - 1999; //minus operator
+console.log(ageHritik);
+const ageSarah = 2018 - 1998;
+console.log(ageSarah);
+
+const now = 20;
+const ageJohn = now + 3; //plus
+console.log(ageJohn);
+
+let ageAyush = 13;
+ageAyush = ageAyush * 2; //multiply
+console.log(ageAyush);
+
+let ageShushi = 32;
+ageShushi = ageShushi / 2; //divide
+console.log(ageShushi);
+
+// ---------------------------- //
+
+// Assignment opeartors
+
+let x = 10 + 5; //15
+x += 10; //25
+x *= 4; //100
+x++; //101
+x--; //100
+console.log(x); //100
---
+++ start1/part1.js
@@ -0,0 +1,6 @@
+let country = "india";
+let continent = "Asia";
+let population = "141 cr";
+console.log(country);
+console.log(continent);
+console.log(population);
