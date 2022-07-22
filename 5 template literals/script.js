670e0522d050cd86fece164031ea99e7c336f994
hritikd3 <hritikdangi3@gmail.com>
2022-07-22 09:42:16 +0530

    part template lietrals

---
+++ 5 template literals/script.js
@@ -0,0 +1,21 @@
+const firstName= "hritik";
+const age= 21;
+const job= 'developer';
+
+// Aam Zindagi
+const print= "I'm " + firstName +", and I'm " + age + " years old !"
+console.log(print)
+
+// Mentos life
+const printNew= `I'm ${firstName} , and I'm ${age} years old`;
+console.log(printNew)
+
+
+console.log(`just a regular string..`)
+
+console.log(" string with \n multiple \n lines")
+
+console.log(` string
+using 
+template 
+literal`)
