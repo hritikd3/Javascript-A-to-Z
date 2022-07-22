592c9f1cdb24484c3758d4d6674974d42e23131a
hritikd3 <hritikdangi3@gmail.com>
2022-07-22 11:47:11 +0530

    feat: type coersion

---
+++ type coersion/script.js
@@ -0,0 +1,13 @@
+
+// Type coercion is the automatic or implicit conversion of values from one data type to another
+
+console.log(" I'm " + 23 + 'years old' )
+console.log('23' + '3' - '4'+ '2' ) // 2292  //in JS the string gets concatenate
+
+console.log('23' /2) //11.5
+console.log(2 * 3) //6
+
+
+let n= '1' +'2'
+n -=1;
+console.log(n)   //11
