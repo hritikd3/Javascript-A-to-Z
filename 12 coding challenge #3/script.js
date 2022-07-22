6e7b9775e0ad99fec40ec05e693fca263a72f368
hritikd3 <hritikdangi3@gmail.com>
2022-07-22 15:25:15 +0530

    challenge#3 complerted

--- 12 coding challenge #3/script.js
+++ 12 coding challenge #3/script.js
@@ -1,18 +1,48 @@
-const dolphinScore1=96;
-const dolphinScore2=108;
-const dolphinScore3=89;
+// const dolphinScore1=96;
+// const dolphinScore2=108;
+// const dolphinScore3=89;
 
-const koalaScore1=88;
-const koalaScore2= 91;
-const koalaScore3= 110;
+// const koalaScore1=88;
+// const koalaScore2= 91;
+// const koalaScore3= 110;
 
-const averageDolphin = (dolphinScore1+dolphinScore2 + dolphinScore3)/3
-const averagekoala = (koalaScore1 + koalaScore2 + koalaScore3) / 3;
-console.log(averageDolphin, averagekoala)
+// const averageDolphin = (dolphinScore1+dolphinScore2 + dolphinScore3)/3
+// const averagekoala = (koalaScore1 + koalaScore2 + koalaScore3) / 3;
+// console.log(averageDolphin, averagekoala)
 
-if( averageDolphin > averagekoala){
-    console.log("Dolphin is the winner" )
-}
-else{
-    console.log("Koala is the winner ")
+// if( averageDolphin > averagekoala){
+//     console.log("Dolphin is the winner" )
+// }
+// else if (averagekoala > averageDolphin){
+//     console.log("Koala is the winner ")
+// }else{
+//     console.log( "both arw winner")
+// }
+
+
+// BONUS 1
+
+// const averageDolphin = (97 + 112 + 101) / 3;
+// const averagekoala = (109 + 95 + 123) / 3;
+// if (averageDolphin > averagekoala && averageDolphin >=100) {
+//   console.log("Dolphin is the winner");
+// } else if (averagekoala > averageDolphin && averagekoala >=100) {
+//   console.log("Koala is the winner ");
+// } else if(averageDolphin ==averagekoala) {
+//   console.log("both arw winner");
+// }
+
+
+// BONUS 2
+
+const averageDolphin = (97 + 112 + 80) / 3;
+const averagekoala = (109 + 95 + 50) / 3;
+if (averageDolphin > averagekoala && averageDolphin >= 100) {
+  console.log("Dolphin is the winner");
+} else if (averagekoala > averageDolphin && averagekoala >= 100) {
+  console.log("Koala is the winner ");
+} else if (averageDolphin == averagekoala && averageDolphin>=100  && averagekoala >=100) {
+  console.log("both arw winner");
+}else{
+    console.log("no one wins the trophy")
 }
