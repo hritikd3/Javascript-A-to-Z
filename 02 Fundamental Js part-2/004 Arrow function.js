3a653b1110179d75ce6e5842374cd27e572c2167
hritikd3 <hritikdangi3@gmail.com>
2022-08-12 23:57:54 +0530

    arrow functions

--- 02 Fundamental Js part-2/004 Arrow function.js
+++ 02 Fundamental Js part-2/004 Arrow function.js
@@ -5,3 +5,11 @@
 const checkAge = birthYear => 2022- birthYear;
 const age= checkAge(2001)
 console.log(age )
+
+//arrow function
+const isRetired=(birthYear, firstName)=>{
+const age= 2022-birthYear;
+const retirementAge= 60-age;
+return `${firstName} retires in ${retirementAge} years`
+}
+console.log(isRetired(2001,'hritik'))
