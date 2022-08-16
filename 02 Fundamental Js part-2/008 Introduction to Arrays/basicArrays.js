32da0f1d9b27bfc6732949ddfb91b77d70e48f42
hritikd3 <hritikdangi3@gmail.com>
2022-08-16 11:31:52 +0530

    initial array

--- 02 Fundamental Js part-2/008 Introduction to Arrays/basicArrays.js
+++ 02 Fundamental Js part-2/008 Introduction to Arrays/basicArrays.js
@@ -7,6 +7,6 @@ const year= new Array(2001,2002,2003,2004,2005)
 console.log(year)
 
 // accessing the values in array
-console.log(fruits[0])  //programmers life starts from  zero . lol 😅
+console.log(fruits[0])  //programmers life starts from  zero . lol 😅!
 console.log(fruits[1])
 console.log(year[2])
