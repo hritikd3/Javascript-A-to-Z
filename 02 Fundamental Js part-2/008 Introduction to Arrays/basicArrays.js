18e7c4ed2fa35702d03de6776d7d39de5e5cd1db
hritikd3 <hritikdangi3@gmail.com>
2022-08-16 11:30:30 +0530

    initial Array

--- 02 Fundamental Js part-2/008 Introduction to Arrays/basicArrays.js
+++ 02 Fundamental Js part-2/008 Introduction to Arrays/basicArrays.js
@@ -1,2 +1,12 @@
+// defining an Array
 const fruits= ['apple' , 'mango ', 'pineapple'];
 console.log(fruits)
+ 
+//  another way
+const year= new Array(2001,2002,2003,2004,2005)
+console.log(year)
+
+// accessing the values in array
+console.log(fruits[0])  //programmers life starts from  zero . lol 😅
+console.log(fruits[1])
+console.log(year[2])
