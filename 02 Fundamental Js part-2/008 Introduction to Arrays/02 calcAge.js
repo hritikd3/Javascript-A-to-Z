99af7bd9a825bf34216d81efa6f2d1bfda1c2752
hritikd3 <hritikdangi3@gmail.com>
2022-08-16 13:05:32 +0530

    more structured

--- 02 Fundamental Js part-2/008 Introduction to Arrays/02 calcAge.js
+++ 02 Fundamental Js part-2/008 Introduction to Arrays/02 calcAge.js
@@ -5,5 +5,11 @@ const calcAge = function (birthyear) {
 const year = [2001, 2002, 2003, 2004];
 const age1= calcAge(year[0]);
 const age2= calcAge(year[1]);
+const age3= calcAge(year[year.length-1]);
 console.log(age1);
 console.log(age2);
+console.log(age3);
+
+// specifying in array format
+const ages = [calcAge(year[0]), calcAge(year[1]), calcAge(year[year.length-1])];
+console.log(ages)
