b6de6e7bbc08565c42de09169664165c2f9026bc
hritikd3 <hritikdangi3@gmail.com>
2022-08-16 13:01:00 +0530

    array calcAge function

---
+++ 02 Fundamental Js part-2/008 Introduction to Arrays/02 calcAge.js
@@ -0,0 +1,9 @@
+const calcAge = function (birthyear) {
+  return 2022 - birthyear;
+};
+
+const year = [2001, 2002, 2003, 2004];
+const age1= calcAge(year[0]);
+const age2= calcAge(year[1]);
+console.log(age1);
+console.log(age2);
