234e9b1cb308e246429c137eefb573d88aab842d
hritikd3 <hritikdangi3@gmail.com>
2022-08-17 12:21:10 +0530

    printing array using loop

--- 02 Fundamental Js part-2/Iteration The Looping Concept/loopingOverArray.js
+++ 02 Fundamental Js part-2/Iteration The Looping Concept/loopingOverArray.js
@@ -6,6 +6,9 @@ const hritik=[
 const types=[];
 for(let i=0;i< hritik.length; i++){
 console.log(hritik[i], typeof hritik[i])
+
+// 2 ways to print typeof of array
 types[i]= typeof hritik[i]
+// types.push(typeof hritik[i])
 }
 console.log(types)
