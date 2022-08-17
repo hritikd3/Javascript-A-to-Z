2a661cc58a6c188348215a8017dfc8354dda41f1
hritikd3 <hritikdangi3@gmail.com>
2022-08-17 15:49:36 +0530

    nested looping updated

--- 02 Fundamental Js part-2/Iteration The Looping Concept/loopingBackward.js
+++ 02 Fundamental Js part-2/Iteration The Looping Concept/loopingBackward.js
@@ -1,6 +1,16 @@
 const array=['hritik',99,'singh','allIsWell']
 
 for(i=array.length-1;i >=0;i--){
-    console.log(array[i])
+    console.log(i,array[i])
 
+}
+
+
+//Nested looping 
+for(i=0;i<array.length;i++){
+    console.log(`=========Learning counting ${i}`)
+
+    for(let i=0;i<array.length; i++){
+        console.log(`count number : ${i}`)
+    }
 }
