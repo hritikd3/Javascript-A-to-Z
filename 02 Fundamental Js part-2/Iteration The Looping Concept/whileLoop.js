7a158e0acef51e3710f14683d85941128e997f42
hritikd3 <hritikdangi3@gmail.com>
2022-08-17 16:06:24 +0530

    dice role using while loop

--- 02 Fundamental Js part-2/Iteration The Looping Concept/whileLoop.js
+++ 02 Fundamental Js part-2/Iteration The Looping Concept/whileLoop.js
@@ -4,3 +4,14 @@ while(i <=10){  // while loop follow the condition , as long as condition is tru
     console.log(` Printing the counting : ${i}`)
     i++;
 }
+
+let dice= Math.trunc(Math.random() *6 )+1 ;
+
+console.log(dice)
+while(dice !==6){
+    console.log(`you rolled a ${dice}`)
+     dice= Math.trunc(Math.random() *6) +1 ;
+        if( dice ===6){
+            console.log(` you are about to end the loop`)
+        }
+}
