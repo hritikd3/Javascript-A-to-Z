f5db29b5151198debf6b71eec74e5310e45786df
hritikd3 <hritikdangi3@gmail.com>
2022-08-17 11:51:04 +0530

    looping on arrays

---
+++ 02 Fundamental Js part-2/Iteration The Looping Concept/loopingOverArray.js
@@ -0,0 +1,6 @@
+//iterate over the array and print all the values of it
+const hritik=[
+    'hritik', 'Singh',2022-2001,'programmer' ,['madhav, mukund, mradul']
+]
+for(let i=0;i< hritik.length; i++)
+console.log(hritik[i], typeof hritik[i])
