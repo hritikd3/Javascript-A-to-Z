5bab5963db0626f63590b4f5f68df0d4a8f683f8
hritikd3 <hritikdangi3@gmail.com>
2022-08-17 15:58:28 +0530

    while loop learning s

---
+++ 02 Fundamental Js part-2/Iteration The Looping Concept/whileLoop.js
@@ -0,0 +1,6 @@
+
+let i=0;
+while(i <=10){  // while loop follow the condition , as long as condition is true it executes the statement 
+    console.log(` Printing the counting : ${i}`)
+    i++;
+}
