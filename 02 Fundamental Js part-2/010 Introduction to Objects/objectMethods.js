88b2bf110c24f68b252015f23143b8b4c6c5f7d5
hritikd3 <hritikdangi3@gmail.com>
2022-08-17 09:48:04 +0530

    palying with objects

--- 02 Fundamental Js part-2/010 Introduction to Objects/objectMethods.js
+++ 02 Fundamental Js part-2/010 Introduction to Objects/objectMethods.js
@@ -2,13 +2,24 @@
 const hritik={
     firstName:'hritik',
     lastName: 'singh',
+    age:'21',
 birthYear:2001,
+job: 'programmer',
+driverLicense: true,
     friends:['shivam','jay','darshan'],
      calcAge: function(){
         console.log(this) //this refers to current object  
         // return 2022- hritik.birthYear;
-
         return 2022- this.birthYear;
+    },
+    getsummary: function(){
+        return `${this.firstName} is a ${this.age} years old ${
+          this.job
+        } and he has his ${this.driverLicense ? "a" : "no"} driverLicense `;
     }
 }
 console.log(hritik.calcAge())
+
+
+// hritik is a 21 years old programmer and he has (a or no) driverLicense
+console.log(hritik.getsummary())
