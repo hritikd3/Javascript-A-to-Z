bdbd04ca9eb935b092dd3a5f0f229c19a6683f38
hritikd3 <hritikdangi3@gmail.com>
2022-08-17 11:26:29 +0530

    for loop updated

--- 02 Fundamental Js part-2/Iteration The Looping Concept/forLoop.js
+++ 02 Fundamental Js part-2/Iteration The Looping Concept/forLoop.js
@@ -1,3 +1,9 @@
 
 // A for loop is a control flow statement for specifying iteration, which allows code to be executed repeatedly
+// for(initialisation ; condition ; execution)
 
+// console.log(" Repeatation 1")
+for(let i=0;i<=10;i++){
+    console.log(` Repeatation ${i}`)
+    
+}
