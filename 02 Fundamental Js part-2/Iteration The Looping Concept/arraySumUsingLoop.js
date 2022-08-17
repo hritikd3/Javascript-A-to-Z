106498a3b57a9d065b7678d8fb36e98f06ed1711
hritikd3 <hritikdangi3@gmail.com>
2022-08-17 12:28:23 +0530

    completed

---
+++ 02 Fundamental Js part-2/Iteration The Looping Concept/arraySumUsingLoop.js
@@ -0,0 +1,8 @@
+// take array and find difference of year and store it in new array
+const year=[2017,2008,2013,2023];
+const age=[]
+
+for(let i=0;i< year.length;i++){
+    age.push(2037-year[i])
+}
+console.log(age)
