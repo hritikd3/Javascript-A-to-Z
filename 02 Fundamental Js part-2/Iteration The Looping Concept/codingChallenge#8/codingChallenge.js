4a5da6bcc85b0bdbc036d1b36c2604f2e13804f8
hritikd3 <hritikdangi3@gmail.com>
2022-08-18 19:33:03 +0530

    coding challenge 8 completed

--- 02 Fundamental Js part-2/Iteration The Looping Concept/codingChallenge#8/codingChallenge.js
+++ 02 Fundamental Js part-2/Iteration The Looping Concept/codingChallenge#8/codingChallenge.js
@@ -12,5 +12,19 @@ for (let i = 0; i < bills.length; i++) {
   tips.push(tip);
   total.push(tip + bills[i]);
 }
-console.log(tips);
-console.log(total);
+// console.log(tips);
+// console.log(total);
+
+
+function calcAverage(arr){
+let sum=0;
+for(let i=0;i<arr.length;i++){
+    sum += parseInt(arr[i]);
+
+}
+ return sum/ arr.length;
+
+}
+console.log(calcAverage([3,2,5,2]))
+console.log(calcAverage(tips));
+console.log(calcAverage(total));
