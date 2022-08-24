d3f4e2e50f6419571936d6ec581e0ac0c0faa2bb
hritikd3 <hritikdangi3@gmail.com>
2022-08-25 00:44:54 +0530

    implemented the functionality for modal

--- 18 project- Modal window/app.js
+++ 18 project- Modal window/app.js
@@ -1,1 +1,24 @@
-'use-strict'
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeBtnModal = document.querySelector('.close-modal');
+const openBtnModal = document.querySelectorAll('.show-modal');
+console.log(openBtnModal);
+
+const openModal = function () {
+    console.log('button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+function closeModal() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+for (let index = 0; index < openBtnModal.length; index++) {
+    openBtnModal[index].addEventListener('click',openModal);
+}
+
+
+closeBtnModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
