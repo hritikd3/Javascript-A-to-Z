4a6d5e91d157fc641bb049629356812ae979815b
hritikd3 <hritikdangi3@gmail.com>
2022-08-25 15:37:36 +0530

    functionality refactored

--- 18 project- Modal window/app.js
+++ 18 project- Modal window/app.js
@@ -7,18 +7,25 @@ const openBtnModal = document.querySelectorAll('.show-modal');
 console.log(openBtnModal);
 
 const openModal = function () {
-    console.log('button clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
+  console.log('button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 };
 function closeModal() {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 }
 for (let index = 0; index < openBtnModal.length; index++) {
-    openBtnModal[index].addEventListener('click',openModal);
+  openBtnModal[index].addEventListener('click', openModal);
 }
-
 
 closeBtnModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  // console.log(e.key)
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
