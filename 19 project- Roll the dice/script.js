eb9e7e813a12378790e952da9c80a5c3272a59d7
hritikd3 <hritikdangi3@gmail.com>
2022-08-26 00:43:24 +0530

    initial functionality

--- 19 project- Roll the dice/script.js
+++ 19 project- Roll the dice/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// selecting the elements from html and referencing them in variable
+const score0 = document.querySelector('#score--0');
+const score1 = document.getElementById('score--1');
+const dice = document.querySelector('.dice');
+score0.textContent = 0;
+score1.textContent = 0;
+
+dice.classList.add('hidden');
