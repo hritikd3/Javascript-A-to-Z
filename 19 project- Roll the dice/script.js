cb419f712d30ec2761e371486c9f567445ac68b2
hritikd3 <hritikdangi3@gmail.com>
2022-08-26 23:59:52 +0530

    functionality implementation

--- 19 project- Roll the dice/script.js
+++ 19 project- Roll the dice/script.js
@@ -87,7 +87,7 @@ btnHold.addEventListener('click', function () {
     document.querySelector(`#score--${activePlayer}`).textContent =
       scores[activePlayer];
     // 2.  check if player score is >=100
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 100) {
       playing = false; //disabling the buttons if any one won .
       // finish the game
       document
