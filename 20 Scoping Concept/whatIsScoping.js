8b6b0df21e78f70de0575d4e930f294f29fb793a
hritikd3 <hritikdangi3@gmail.com>
2022-08-29 14:56:22 +0530

    what is scoping

---
+++ 20 Scoping Concept/whatIsScoping.js
@@ -0,0 +1,30 @@
+// Lexical scoping - scoping is controlled by placement of function and blocks in code.
+
+// scope space or environment in which certain variable is decalared .
+// Three tyoes of scoping 
+// 1) global scope 2) Function scope 3) Block scope 
+
+
+// scope chain-> means that one variable has scope (it may be function, global or block scope) is used by another scope .
+
+// /Example
+const myName= 'hritik' //global scope 
+
+ function first(){
+    const age=21;
+
+    if(age>= 21){
+        const decade=3; //let and const are block scoped 
+        var millenial= true;
+    }
+
+    function second(){
+        const job= ' Developer';
+//here we want variable from first() function and global so it can lookup in scope chain 
+        console.log(`${myName} is a ${age} years old & he is a ${job}`)
+        second();
+    }
+
+ }
+
+
