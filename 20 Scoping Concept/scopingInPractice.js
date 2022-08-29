b0aa95fdf74af42adaa15e9e071b5a033e5ddbaf
hritikd3 <hritikdangi3@gmail.com>
2022-08-29 15:46:11 +0530

    explaining scoping

--- 20 Scoping Concept/scopingInPractice.js
+++ 20 Scoping Concept/scopingInPractice.js
@@ -1,8 +1,27 @@
 'use strict'
 
-function calcAge(birthYear) {
-    const age= 2022- birthYear;
- console.log(firstName)
+// function calcAge(birthYear) {
+//     const age= 2022- birthYear;
+//     console.log(age)
+//  console.log(firstName)
+// }
+// const firstName= " Hritik"
+// calcAge(2001)
+
+function calcAge(birthYear){
+    const age=2022-birthYear;
+
+    function printAge() {
+      const output = ` ${firstName}  your age is ${age} and born in ${birthYear}  `;
+      console.log(output);
+
+      if (year >= 1981 && year <= 1996) {
+        let millenial = true;
+      }
+      console.log(millenial); //reference error
+    }
+    printAge()
+    return age ;
 }
-const firstName= " Hritik"
-calcAge()
+const firstName= "hritik"
+calcAge(1991)
