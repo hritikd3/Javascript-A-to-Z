c6902436fa8d640f3054ce55f75785908eb22a17
hritikd3 <hritikdangi3@gmail.com>
2022-08-31 14:59:30 +0530

    reference type

--- 23 Primitive Vs Object/script.js
+++ 23 Primitive Vs Object/script.js
@@ -1,4 +1,26 @@
-let firstName = 'hritik'
-let lastName = firstName
-lastName = 'singh';
-console.log(lastName , firstName)
+// if we check behind the secen inside the callstack how this works,  so three parameter created ( Identifier , Address , Value)
+/* Identifier   | Address | Value
+   firstName   | 0001    |  hritik
+   lastName   | 0001     | hritik ( pointing to same address)
+  firstName  | 0002    |  singh
+*/
+
+// primtive types
+let firstName = "hritik";
+let lastName = firstName;
+firstName = "singh";
+console.log(firstName, lastName);
+
+// reference type
+const jessica={
+    firstName: "jessica",
+    lastName: 'sharma',
+    age: 21
+};
+
+let friendJessica= jessica;
+friendJessica.lastName= 'singh';
+console.log("before friendship:", jessica);
+console.log("after friendship" , friendJessica)
+
+friendJessica={}
