07593ce76c314f6b07d194f07ab454ee3f59061c
hritikd3 <hritikdangi3@gmail.com>
2022-09-04 17:27:12 +0530

    destructuring in js folder created

---
+++ 24 destructuring in JS/script.js
@@ -0,0 +1,3 @@
+//  destructuring is the feature which is introduced in  ES6 version of JS
+// it is a JavaScript expression that makes it possible to unpack values from arrays, or properties from objects, into distinct variables.
+
