e9a26917009a5d4ae8341b0776f2b911018dd826
hritikd3 <hritikdangi3@gmail.com>
2022-09-04 17:46:33 +0530

    movie object destructuring

---
+++ 24 destructuring in JS/objectDestruct.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const movie = {
+  Title: "First Love, Last Rites",
+  location: ['italy', 'spain','madrid'],
+  "Production Budget": 300000,
+  "Release Date": "Aug 07 1998",
+   Distributor: "Strand",
+  "Major Genre": "Drama",
+  "IMDB Rating": 6.9,
+  "IMDB Votes": 207,
+};
+
+console.log(movie['Title']);
+console.log(movie.Title);
--- 24 destructuring in JS/script.js
+++ 24 destructuring in JS/script.js
@@ -1,3 +1,13 @@
 //  destructuring is the feature which is introduced in  ES6 version of JS
 // it is a JavaScript expression that makes it possible to unpack values from arrays, or properties from objects, into distinct variables.
 
+const arr=[2,3,4];  //declared the array here
+const a=arr[0];    //value at index 0
+const b=arr[1];    //value at index 1
+const c=arr[2];     //value at index 2
+
+const [x,y,z]= arr;  //destructring value from arr 
+console.log(x,y,z);   //x=2 , y=3 , z=4
+console.log(arr);  //x,y,z : 2,3,4
+
+
