4398862f96c04b8797734aac987ee247c9eaaac3
hritikd3 <hritikdangi3@gmail.com>
2022-11-30 15:45:47 +0530

    string indexOf method

---
+++ 24 Data Structure ,operators, string/33 Strings/indexOf.js
@@ -0,0 +1,12 @@
+// The indexOf() method, given one argument: a substring to search for, searches the entire calling string, and returns the index of the first occurrence of the specified substring.
+
+// index starts from 0
+
+const myBook= "How to win Friends and influence people";
+console.log(myBook.indexOf('o'))
+console.log(myBook.indexOf('r'))
+
+console.log(myBook[2])
+console.log(myBook[12])
+console.log("A3Rw".length) // 4
+console.log(myBook.length) // 39
