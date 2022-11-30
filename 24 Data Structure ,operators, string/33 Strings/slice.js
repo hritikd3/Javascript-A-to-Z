78f27d5895aa9cc3f972f41814753ee6a64321f4
hritikd3 <hritikdangi3@gmail.com>
2022-11-30 16:05:42 +0530

    slice method

---
+++ 24 Data Structure ,operators, string/33 Strings/slice.js
@@ -0,0 +1,16 @@
+// // The slice() method extracts a section of a string and returns it as a new string, without modifying the original string.
+
+// FORMAT 
+// slice(indexStart);
+// slice(indexStart, indexEnd);
+const myBook = "How to win Friends and influence people";
+
+console.log(myBook.slice(6))
+console.log("hello how are you ".slice(2))
+
+console.log(myBook.slice(6, 17))  //win Friend
+console.log(myBook.slice(1, -1))  //ow to win Friends and influence peopl
+
+console.log(myBook.slice(-6))  // return from last
+
+// school class prediction 
