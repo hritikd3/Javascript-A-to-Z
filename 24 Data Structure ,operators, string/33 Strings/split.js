3841ef639f175f4009431f26b8236a8a3ffe8a57
hritikd3 <hritikdangi3@gmail.com>
2022-11-30 17:12:22 +0530

    split

--- 24 Data Structure ,operators, string/33 Strings/split.js
+++ 24 Data Structure ,operators, string/33 Strings/split.js
@@ -17,3 +17,17 @@ const names= 'hritik dangi'
 console.log(names.split('  '))  //[ 'hritik dangi' ]
 console.log(names.split(' '))  //[ 'hritik', 'dangi' ]
 console.log(names.split(''))   //[  'h', 'r', 'i', 't',  'i', 'k', ' ', 'd',  'a', 'n', 'g', 'i']
+
+
+const capitalizeName= function (names){
+    const changeName= names.split(' ')
+const nameUpper= [];
+
+for(const n of changeName){
+ nameUpper.push(n.replace(n[0],n[0].toUpperCase()))
+ //or alternate
+// nameUpper.push(n[0].toUpperCase() + n.splice(1))
+};
+console.log(nameUpper.join(' '))
+}
+capitalizeName(' early to bed and early to rise')
