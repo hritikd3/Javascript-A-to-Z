29f2bdb8776807d009d5200972c8766be2813d62
hritikd3 <hritikdangi3@gmail.com>
2022-11-30 17:16:09 +0530

    game play using repeat method

---
+++ 24 Data Structure ,operators, string/33 Strings/repeat.js
@@ -0,0 +1,13 @@
+
+
+const names=" hritik dangi";
+console.log(names.repeat(3)   )
+
+
+// I liked the way i used repeat method this is interesting 😍😄
+const planeArrival= function ( n){
+    console.log(`There are ${n} planes arrived ${'✈️'.repeat(n)} `)
+}
+planeArrival(3)
+planeArrival(5)
+planeArrival(8)
