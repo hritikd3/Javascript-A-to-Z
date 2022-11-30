5b4d9bc02b1b973985b06781e53e1177da15d024
hritikd3 <hritikdangi3@gmail.com>
2022-11-30 23:58:45 +0530

    script.js

---
+++ 24 Data Structure ,operators, string/codingChallenges#4/script.js
@@ -0,0 +1,11 @@
+document.getElementById('button').addEventListener('click',()=>{
+    const text= document.getElementById('textarea').value;
+    console.log(text)
+
+    const rows= text.split('\n');
+    console.log(rows)
+
+    for(const row of rows){
+        
+    }
+})
