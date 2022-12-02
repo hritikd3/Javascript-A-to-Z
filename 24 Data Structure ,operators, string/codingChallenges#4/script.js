931b290d36fdaf662d30dca9b76ed2d13fb5f802
hritikd3 <hritikdangi3@gmail.com>
2022-12-02 10:45:58 +0530

    done with CC 4

--- 24 Data Structure ,operators, string/codingChallenges#4/script.js
+++ 24 Data Structure ,operators, string/codingChallenges#4/script.js
@@ -1,14 +1,14 @@
 document.getElementById('button').addEventListener('click',()=>{
     const text= document.getElementById('textarea').value;
-    console.log(text)
+    // console.log(text)
 
     const rows= text.split('\n');
-    console.log(rows)
+    // console.log(rows)
 
-    for(const row of rows){
+    for(const [i,row] of rows.entries()){
         const [first,second]=row.toLowerCase().trim().split('_');
-console.log(row, first , second)
+// console.log(row, first , second)
         const output= `${first}${second.replace(second[0], second[0].toUpperCase())}`
-        console.log(output)
+        console.log(` ${output.padEnd(20)} ${'✅'.repeat(i+1)}`)
     }
 })
