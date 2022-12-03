46ae4d2d7e3400352df386d5ba4cde29fd19073a
hritikd3 <hritikdangi3@gmail.com>
2022-12-03 23:36:38 +0530

    meanwhile

--- 34 Intro to Functions/IIFE.js
+++ 34 Intro to Functions/IIFE.js
@@ -1,3 +1,16 @@
 // WHAT IS IIRFilterNode( inmmeediately invoked function )
 
 // An IIFE (Immediately Invoked Function Expression) is a JavaScript function that runs as soon as it is defined. 
+
+const runOnce= function (){
+    console.log(' This will run once only ')
+}
+ runOnce();
+
+ //here if we remove open braces so it will throw error , because we directly define the function rather we need to make it function expression which is possible when we have () at start   i.e, (function() ....)
+ (function(){
+    console.log('this will run once')
+ }) ();
+
+ // one more example 
+ (()=>{console.log(' this is running ')})();
