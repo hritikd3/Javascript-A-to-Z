202a9f3f650d63dd879ca5143d865dafdc85df5e
hritikd3 <hritikdangi3@gmail.com>
2022-12-04 23:29:03 +0530

    good example added for apply() method

--- 34 Intro to Functions/applyMethod.js
+++ 34 Intro to Functions/applyMethod.js
@@ -29,3 +29,11 @@ const person1={
 }
 console.log(person.fullName.apply(person1,['Indore', ' MP']))
 
+
+// another example 
+
+const array = ["a", "b"];
+const elements = [0, 1, 2];
+array.push.apply(array, elements);
+console.info(array); // ["a", "b", 0, 1, 2]
+
