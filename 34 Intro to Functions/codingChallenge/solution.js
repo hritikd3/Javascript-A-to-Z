ee29aada00d94ac2859a0f45a770965b6a91932b
hritikd3 <hritikdangi3@gmail.com>
2022-12-22 19:39:06 +0530

    solution implemented for challenge

---
+++ 34 Intro to Functions/codingChallenge/solution.js
@@ -0,0 +1,14 @@
+const poll = {
+ question: "What is your favourite programming language?",
+ options: ["0: JavaScript", "1: Python", "2: Rust", "3:  C++"],
+ answers: new Array(4).fill(0),
+
+ registerNewAnswer(){
+    //get answer
+    const newAnswer=prompt(`${this.question}\n ${this.options.join('\n')}\n (write option number)`)
+    console.log(newAnswer)
+  //register answer
+  
+} 
+};
+poll.registerNewAnswer();
