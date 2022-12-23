d45403e543567bc14bda2ad8efea4aee4dd8a80c
hritikd3 <hritikdangi3@gmail.com>
2022-12-23 15:33:12 +0530

    closures explained with example

---
+++ 34 Intro to Functions/closures.js
@@ -0,0 +1,19 @@
+// A CLOSURE IS CLOSED  OVER VARIABLE ENIRONMENT OF EXCUTION CONTEXT N WHICH FUNCTION WAS CREATED EVEN AFTER EXCUTION CONTEXT IS GONE
+// OR -> A CLOSURE IS FUNCTION WITH ITS LEXICAL ENVIRONMENT WHEN BULDLED TOGETHER IS KNOWN AS CLOSURES
+
+const secureBooking= function (){
+    let passenger=0;
+
+    return function(){  //it is acting as closures
+  passenger++;
+  console.log(`${passenger} Passenger`)
+    }
+}
+
+const booker= secureBooking();
+
+
+// this function can increamenting passenger value just because of closures
+booker();  //1 Passenger
+booker();  //2 Passenger
+booker();  //3 Passenger
