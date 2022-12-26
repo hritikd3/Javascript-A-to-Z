1679a07dd33ad7a6b33eb3c66d57c7c34c8bdc0d
hritikd3 <hritikdangi3@gmail.com>
2022-12-26 21:34:32 +0530

    splice mthod

---
+++ 35 Arrays/sliceMethod.js
@@ -0,0 +1,8 @@
+// The slice() method returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included) where start and end represent the index of items in that array. The original array will not be modified.
+
+const arr= ['a','b','c','d','e'];
+
+console.log(arr.slice(2))
+console.log(arr.slice(2,4))
+console.log(arr.slice(-2))
+console.log(arr.slice()) // 
