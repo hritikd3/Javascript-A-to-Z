85334c8bc4e3b098686edae6b4b5a5848c5f8484
hritikd3 <hritikdangi3@gmail.com>
2022-12-26 23:34:03 +0530

    concat and join method

---
+++ 35 Arrays/concatMethod.js
@@ -0,0 +1,11 @@
+const array1 = ["a", "b", "c"];
+const array2 = ["d", "e", "f"];
+const array3 = array1.concat(array2);
+
+console.log(array3);
+// expected output: Array ["a", "b", "c", "d", "e", "f"]
+
+
+//join
+const arr = ["a", "b", "c"];
+console.log(arr.join(' - '))
