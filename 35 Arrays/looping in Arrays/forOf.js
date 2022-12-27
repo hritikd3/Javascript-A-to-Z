aac1de75e9144fb164a22fae01d759ae7035fcba
hritikd3 <hritikdangi3@gmail.com>
2022-12-27 18:45:45 +0530

    for of implmented using diff way

--- 35 Arrays/looping in Arrays/forOf.js
+++ 35 Arrays/looping in Arrays/forOf.js
@@ -9,3 +9,14 @@ for(const movement of movements){
     console.log(`you withdraw ${Math.abs(movement)}`)
     }
 }
+
+console.log("*********using different way**********")
+
+const arr = [1200, 300, -120, 900, 220, -412, 730];
+for(const [i,value] of arr.entries()){
+     if(value > 0){
+        console.log(`value ${i+1}: you deposited  ${value}`)
+    }else{
+    console.log(`value ${i+1}: you withdraw ${Math.abs(value)}`)
+    }
+}
