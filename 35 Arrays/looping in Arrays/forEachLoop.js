f1afe2780a93ff5b030053546982fe83772395fd
hritikd3 <hritikdangi3@gmail.com>
2022-12-27 10:03:47 +0530

    prog to get moements from bill

--- 35 Arrays/looping in Arrays/forEachLoop.js
+++ 35 Arrays/looping in Arrays/forEachLoop.js
@@ -2,7 +2,8 @@ console.log(" USING FOR EACH")
 
 const movements = [1200, 300, -120, 900, 220, -412, 730];
 
-movements.forEach(function(movement){
+// FOR EACH ACT AS HIGHER ORDER FUNCTION AND THE FUNCTION UNDER IT ACT AS CALLBACK FUNCTION 
+movements.forEach(function(movement){  
   if (movement > 0) {
     console.log(`you deposited  ${movement}`);
   } else {
