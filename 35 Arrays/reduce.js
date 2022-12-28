a282b553e8890c09352eff55ca1233229e179d93
hritikd3 <hritikdangi3@gmail.com>
2022-12-29 00:01:55 +0530

    reduce using arrow function

--- 35 Arrays/reduce.js
+++ 35 Arrays/reduce.js
@@ -3,5 +3,10 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 const totalBalance= movements.reduce(function(acc,cur,i){
     console.log(`Iteration ${i}: ${acc}`)
     return acc+cur;
-},0)
+},0)  //this 0 is for the start accumulator kaha se karna 
 console.log(totalBalance)
+
+
+//alternate using arrow function
+const balance= movements.reduce((acc,cur)=> acc+ cur,0)
+console.log(balance)
