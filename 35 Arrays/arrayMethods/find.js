878bdaf78dc15feb72ed7d2a6a7a5be237e40852
hritikd3 <hritikdangi3@gmail.com>
2022-12-30 16:20:43 +0530

    find method in detail

--- 35 Arrays/arrayMethods/find.js
+++ 35 Arrays/arrayMethods/find.js
@@ -6,3 +6,43 @@ const firstWithdrawal = movements.find((mov) => mov < 0);
 console.log(firstWithdrawal);
 
 
+
+console.log(" ***********************")
+console.log(" Task is to find the required owner name from many objects ")
+
+// Data
+const account1 = {
+  owner: 'Hritik Dangi',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Madhav Mishra',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Nikhil Kumar',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+
+
+const account = accounts.find(
+  (acc) => acc.owner === "Madhav Mishra" );
+console.log(account)  //so it return the object which have name Madhav Mishra
