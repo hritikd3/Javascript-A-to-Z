3fb2f79705d176e39f07b2052225f8d17ba4d153
hritikd3 <hritikdangi3@gmail.com>
2023-03-04 23:39:30 +0530

    implemented using example

--- 35 Arrays/arrayMethods/flat&flatMap.js
+++ 35 Arrays/arrayMethods/flat&flatMap.js
@@ -1,1 +1,17 @@
 // flat method works best with  nested arrays 
+
+const arr= [[1,2,3],4,5,[6,7,8]]
+const arr1= [[9,1,[1,2,3],4,5,[6,7,8]]]
+
+console.log(arr.flat())
+console.log(arr1.flat(2))
+
+//flat map 
+//The flatMap() method uses a combination of the map() and flat() methods to perform operations.
+
+const arr4 = [1, 2, [3, [4, 5, [6, 7]]]];
+
+console.log(arr4.flatMap((element) => element).flat(2));
+
+// expected output
+// [1, 2, 3, 4, 5, 6, 7];
