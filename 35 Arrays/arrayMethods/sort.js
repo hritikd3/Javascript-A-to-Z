c43186cee3f1935933450b6f8d3f9148c7366b8d
hritikd3 <hritikdangi3@gmail.com>
2023-03-04 23:46:38 +0530

    sort method

---
+++ 35 Arrays/arrayMethods/sort.js
@@ -0,0 +1,11 @@
+// The sort() method sorts the elements of an array in place and returns the reference to the same array, now sorted. The default sort order is ascending, built upon converting the elements into strings, then comparing their sequences of UTF-16 code units values.
+
+const fruits= ['zanar','papaya', 'apple', "grapes", 'orange '];
+fruits.sort()
+console.log(fruits) // sorted on the basis of string 
+
+
+
+//lets sort number  
+const numSort= [12,22,400,7,9,-23,-09]
+console.log(numSort.sort())  // noty sorted in order
